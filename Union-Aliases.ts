57f4d91a08d198015dc8a792b1ba800d56b7c979
Jatin Tilwani <jatintilwani500@gmail.com>
2024-06-02 12:14:17 +0530

    tsconfig.json

--- Union-Aliases.ts
+++
@@ -1,8 +0,0 @@
-//type - aliases 
-
-type combineable = number | string
-
-function add (a: combineable, b:combineable){
-    let result = a + b
-    return result
-}
--- basics.ts
+++
@@ -1,8 +0,0 @@
-function add(a: number, b: number, showResult : boolean) {
-    if(showResult) return a + b;
-    return -1
-}
-
-const printResult = true;
-
-console.log(add(2, 2.8, printResult));
